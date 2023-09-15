89fb323d9c1b46ed5cde0a84c673b5d4c7d19a12
Vickyvit <131874692+Vickyvit@users.noreply.github.com>
2023-09-15 23:48:22 +0530

    Add files via upload

---
+++ notes  /backtracking/permutation_3.java
@@ -0,0 +1,22 @@
+public class permutation_3 {
+
+    public static void permutation(String str, String ans){
+        //base case
+        if(str.length() == 0){
+            System.out.println(str);
+            return;
+        }
+        //Recursion
+        for(int i= 0 ; i < str.length(); i++){
+        char curr = str.charAt(i);
+        String newstr = str.substring(0, i)+ str.substring(i+1);
+        permutation(newstr, ans + curr);
+    }
+}
+
+    public static void main( String args[]){
+        String str = "abc";
+        permutation(str, "");
+    }
+}
+
